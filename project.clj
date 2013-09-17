8cf809dd13fa2abea1bae92e2d2382befbeb7cb7
Justin Carr <justin.carr@puppetlabs.com>
2013-09-16 23:02:11 -0700

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,26 @@
+(defproject jsite "1.0.0-SNAPSHOT"
+  :description "Personal website"
+  :url "http://jcarr.herokuapp.com"
+  :license {:name "FIXME: choose"
+            :url "http://example.com/FIXME"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]
+                 [ring/ring-jetty-adapter "1.1.0"]
+                 [ring/ring-devel "1.1.0"]
+                 [ring-basic-authentication "1.0.1"]
+                 [environ "0.2.1"]
+                 [com.cemerick/drawbridge "0.0.6"]
+                 ;; json 
+                 [cheshire "5.2.0"]
+                 ;; Routing library
+                 [compojure "1.1.5"]
+                 ;; Templating for Cojure
+                 [enlive "1.1.1"]
+                 ;; HTML in Clojure
+                 [hiccup "1.0.4"]
+                 ;; HTML->Hiccup
+                 [hickory "0.4.1"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks [environ.leiningen.hooks]
+  :profiles {:production {:env {:production true}}})
---
+++ test/jsite/web_test.clj
@@ -0,0 +1,6 @@
+(ns jsite.web-test
+  (:require [clojure.test :refer :all]
+            [jsite.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
